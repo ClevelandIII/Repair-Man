fb9d33a5792778a5358d483f89eb33b220ce3551
Sean Zimbelman <Szimbe295@west-mec.local>
2021-08-27 17:50:52 -0700

    nav bar base

---
+++ repair/src/App.jsx
@@ -0,0 +1,32 @@
+import { links } from "./utils/const.js";
+import { Route, Switch } from "react-router-dom";
+import { About, Contact, Home, Error } from "./pages";
+import { Navbar } from "./components/Navbar.jsx";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        {links
+        .filter((link) => link.text !== 'home' )
+        .map((link) => {
+          const { id, url, page } = link;
+          return (
+            <Route key={id} path={url}>
+              {page}
+            </Route>
+          );
+        })}
+        {/* <Route exact path='/*'>
+        <Error/>
+      </Route> */}
+      </Switch>
+    </>
+  );
+}
+
+export default App;
