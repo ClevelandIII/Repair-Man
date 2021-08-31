bc7d2ce37d31e7d05a23a99559000e60d82a2ef0
Sean Zimbelman <Szimbe295@west-mec.local>
2021-08-30 18:25:09 -0700

    home css

--- repair/src/components/Picture.jsx
+++ repair/src/components/Picture.jsx
@@ -1,16 +1,37 @@
 import React, { useState, useEffect } from "react";
 
 const Picture = ({ img, name, info, side }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <>
       {side === "left" ? (
         <div className="picture">
           <img src={img} alt={name} />
-          <p>{info}</p>
+          <p>
+            {" "}
+            {readMore ? info : `${info.substring(0, 200)}... `}
+            <button
+              onClick={() => {
+                setReadMore(!readMore);
+              }}
+            >
+              {readMore ? "show less" : "show more"}
+            </button>
+          </p>
         </div>
       ) : (
         <div className="picture">
-          <p>{info}</p>
+          <p>
+            {" "}
+            {readMore ? info : `${info.substring(0, 200)}... `}
+            <button
+              onClick={() => {
+                setReadMore(!readMore);
+              }}
+            >
+              {readMore ? "show less" : "show more"}
+            </button>
+          </p>
           <img src={img} alt={name} />
         </div>
       )}
