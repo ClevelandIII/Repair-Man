7d492554b5e747f7cd204a82e91e579e1a1b642b
Cleveland Williams <Cwilli290@west-mec.local>
2021-09-01 16:59:14 -0700

    photo finish

--- repair/src/pages/Contact.jsx
+++ repair/src/pages/Contact.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
     <div>
-      Contact Page
+      <form action="">
+        <div className="">
+          <div className="fName"> enter first name</div>
+          <div className="Lname"> enter last name</div>
+        </div>
+        <div className="question">
+          <input type="text" placeholder="text"/>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- repair/src/pages/Photos.jsx
+++ repair/src/pages/Photos.jsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import { photoData } from "../utils/data";
 
 const Photos = () => {
-  return (
+  return <>
+      <h1 className="title">Photo Gallery</h1>
       <div className="photos">
         {photoData.map((info) => {
           const { image } = info;
           return <img src={image} alt="Given" className="image" />;
         })}
       </div>
-  );
+  </>;
 };
 
 export default Photos;
