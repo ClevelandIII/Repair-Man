8c023929e377bbaf0a6988345fd7c183bc2799c8
Sean Zimbelman <Szimbe295@west-mec.local>
2021-08-31 17:14:59 -0700

    reactive design

--- repair/src/components/Picture.jsx
+++ repair/src/components/Picture.jsx
@@ -2,14 +2,64 @@ import React, { useState, useEffect } from "react";
 
 const Picture = ({ img, name, info, side }) => {
   const [readMore, setReadMore] = useState(false);
+  const [words, setWords] = useState(200);
+
+  const screenResize = () => {
+    if (window.innerWidth < 500) {
+      setWords(50);
+    } else if (window.innerWidth >= 500 && window.innerWidth < 600) {
+      setWords(100);
+    } else if (window.innerWidth >= 600 && window.innerWidth < 700) {
+      setWords(150);
+    } else if (window.innerWidth >= 700 && window.innerWidth < 800) {
+      setWords(200);
+    } else if (window.innerWidth >= 800 && window.innerWidth < 900) {
+      setWords(250);
+    } else if (window.innerWidth >= 900 && window.innerWidth < 1000) {
+      setWords(300);
+    } else if (window.innerWidth >= 1000 && window.innerWidth < 1100) {
+      setWords(400);
+    } else {
+      setWords(500);
+    }
+  };
+
+  useEffect(() => {
+    screenResize();
+    // set resize listener
+    window.addEventListener("resize", screenResize);
+
+    // clean up function
+    return () => {
+      // remove resize listener
+      window.removeEventListener("resize", screenResize);
+    };
+  }, []);
+
+  if (info.length <= words) {
+    return (
+      <>
+        {side === "left" ? (
+          <div className="picture">
+            <img src={img} alt={name} />
+            <p>{info}</p>
+          </div>
+        ) : (
+          <div className="picture">
+            <p>{info}</p>
+            <img src={img} alt={name} />
+          </div>
+        )}
+      </>
+    );
+  }
   return (
     <>
       {side === "left" ? (
         <div className="picture">
           <img src={img} alt={name} />
           <p>
-            {" "}
-            {readMore ? info : `${info.substring(0, 200)}... `}
+            {readMore ? `${info}... ` : `${info.substring(0, words)}... `}
             <button
               onClick={() => {
                 setReadMore(!readMore);
@@ -22,8 +72,7 @@ const Picture = ({ img, name, info, side }) => {
       ) : (
         <div className="picture">
           <p>
-            {" "}
-            {readMore ? info : `${info.substring(0, 200)}... `}
+            {readMore ? `${info} ... ` : `${info.substring(0, words)}... `}
             <button
               onClick={() => {
                 setReadMore(!readMore);
