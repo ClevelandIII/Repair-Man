7cb1f921a76c7a3f4acb421cebf183e0d6419865
Cleveland Williams <Cwilli290@west-mec.local>
2021-09-01 17:22:45 -0700

    photo finish and temp contact

--- repair/src/pages/Contact.jsx
+++ repair/src/pages/Contact.jsx
@@ -3,14 +3,16 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <form action="">
-        <div className="">
-          <div className="fName"> enter first name</div>
-          <div className="Lname"> enter last name</div>
+      <h1>Contact Me</h1>
+      <form action="submit">
+        <div className="nforums">
+          <input type="text" className="fname" placeholder="enter first name"/>
+          <input type="text"className="lname" placeholder="enter last name"/>
         </div>
         <div className="question">
-          <input type="text" placeholder="text"/>
+          <input type="text" placeholder="text" className="questinput"/>
         </div>
+        <button>Send the Message</button>
       </form>
     </div>
   );
