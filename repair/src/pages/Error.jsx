b256a8f77fdcf62707c33e9fad96126d577b19d5
Cleveland Williams <Cwilli290@west-mec.local>
2021-09-01 18:24:19 -0700

    conflict 2

--- repair/src/pages/Error.jsx
+++ repair/src/pages/Error.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Error = () => {
   return (
-    <div className="error">
-      <div className="text"> 404 Page Not Found</div>
+    <div className="err">
+      <h1 className="txt">404 PAGE NOT FOUND</h1>
     </div>
   );
-};
+}; 
 
 export default Error;
